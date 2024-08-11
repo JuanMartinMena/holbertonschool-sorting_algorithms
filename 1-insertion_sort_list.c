ece551dc62e1a4ea9a2faacf689cd0a2496c56e9
JuanMartinMena <juanmartinmenac@gmail.c>
2024-08-11 18:18:01 +0000

    fix compile program

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,15 +8,15 @@
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
 	if (node1->prev)
-		node->prev_next = node2;
+		node1->prev->next = node2;
 	else
 		*list = node2;
 	if (node2->next)
 		node2->next->prev = node1;
 	node1->next = node2->next;
-	node2->prev = node1->prev;
-
 	node2->next = node1;
+
+	node2->prev = node1->prev;
 	node1->prev = node2;
 
 	print_list(*list);
